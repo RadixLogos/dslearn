198842ce15965107bba7fd36640a717a0f03e384
rubens <rubenshds@gmail.com>
2026-02-27 10:55:14 -0300

    Notification entity created

--- src/main/java/com/devsuperior/dslearn/entities/User.java
+++ src/main/java/com/devsuperior/dslearn/entities/User.java
@@ -13,11 +13,14 @@ public class User {
     private String name;
     private String email;
     private String password;
+
     @ManyToMany
     @JoinTable(name = "tb_user_role",
             joinColumns = @JoinColumn(name = "user_id",referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name ="role_id",referencedColumnName = "id"))
     private Set<Role> roles = new HashSet<>();
+    @OneToMany(mappedBy = "user")
+    private Set<Notification> notifications = new HashSet<>();
 
     public User() {
     }
@@ -27,5 +30,53 @@ public class User {
         this.name = name;
         this.email = email;
         this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void addRoles(Role role) {
+        this.roles.add(role);
+    }
+
+    public Set<Notification> getNotifications() {
+        return notifications;
+    }
+
+    public void addNotifications(Notification notification) {
+        this.notifications.add(notification);
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -14,3 +14,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 
 INSERT INTO tb_course (name,img_uri, img_gray_uri) VALUES ('Curso de HTML','https://www.devmedia.com.br/arquivos/cursos/HTML_fundamentos/thumb.webp','https://julianolopes.com.br/wp-content/uploads/2021/02/simbolo-html-300x300.jpeg');
 INSERT INTO tb_offer (edition,start_moment,end_moment,course_id) VALUES ('First',TIMESTAMP WITH TIME ZONE'2025-02-12T12:30:00Z',TIMESTAMP WITH TIME ZONE'2026-02-12T12:30:00Z',1);
+
+INSERT INTO tb_notification (text,moment,read,route,user_id) VALUES ('Você ingressou no curso de HTML!',TIMESTAMP WITH TIME ZONE'2025-02-12T12:30:00Z',false,'https://localhost:8080/user/1',1);
